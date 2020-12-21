f5eae8a89c6f944824bff497b9c578d5dd92b67e
osmosishk <tonyho@osmosis.com.hk>
2020-12-21 22:24:28 +0800

    test

--- src/layouts/layout-components/footer/Footer.js
+++ src/layouts/layout-components/footer/Footer.js
@@ -5,7 +5,7 @@ const Footer = () => {
     return (
         <footer className="footer text-center">
             All Rights Reserved by Osmsois admin. Designed and Developed by{' '}
-            <a href="https://wrappixel.com">Osmosis</a>.
+            <a href="https://wrappixel.com">Osmosis System</a>.
         </footer>
     );
 }
