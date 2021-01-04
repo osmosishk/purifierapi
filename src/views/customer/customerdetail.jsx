6d085cc12cc01a198c63931da4444f8e540fa722
osmosishk <tonyho@osmosis.com.hk>
2021-01-04 22:18:09 +0800

    4 jans changs

--- src/views/customer/customerdetail.jsx
+++ src/views/customer/customerdetail.jsx
@@ -23,15 +23,44 @@ class Customerdetail extends Component {
         
         
      } 
-
-     componentDidMount() {
+    componentWillMount() {
+            this.getMachine();
+            this.getCustomer();
+            this.getCase();
         
-        this.getCustomer();
-      
-    }
+        }
+     async getMachine()
+     {
+        
+        const headers = {'Authorization': 'token c3c1d72b219561cfe00084d3434f37c3714f5961' }
+        await axios.get(config.getMachineonlyclient, {headers: headers ,params: {cid: this.state.id} })
+            .then((response) => {
+              
+            this.setState({machine:response.data});
+        });
+        
+        
+     } 
+
+     async getCase()
+     {
+        
+        const headers = {'Authorization': 'token c3c1d72b219561cfe00084d3434f37c3714f5961' }
+        await axios.get(config.getCaseonlyclient, {headers: headers ,params: {cid: this.state.id} })
+            .then((response) => {
+              
+            this.setState({case:response.data});
+        });
+        
+        
+     } 
+
+    
     render() { 
         
         console.log(this.state.customerinfo)
+        console.log(this.state.machine)
+        console.log(this.state.case)
 
         return ( 
              <div><h1>Customer Detail {this.state.id}</h1></div>
