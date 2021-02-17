e46353515a99776287cc788699a143a71ddd41c2
osmosishk <tonyho@osmosis.com.hk>
2021-02-17 21:48:55 +0800

    20210217

--- src/views/customer/customerdetail.jsx
+++ src/views/customer/customerdetail.jsx
@@ -64,6 +64,183 @@ class Customerdetail extends Component {
 
         return ( 
              <div><h1>Customer Detail {this.state.id}</h1></div>
+
+             <div className="details-table px-4">
+            <Table responsive borderless size="sm" className="mt-4">
+              <tbody>
+                <tr className="d-flex">
+                  <td className="col-3 font-bold">First Name</td>
+                  <td className="col-9">
+                    {editContactFlag ? (
+                      <Input
+                        type="text"
+                        name="firstName"
+                        id="firstName"
+                        defaultValue={selectedContacts.firstname}
+                        onChange={(e) =>
+                          dispatch(
+                            updateContact(
+                              selectedContacts.id,
+                              "firstname",
+                              e.target.value
+                            )
+                          )
+                        }
+                      />
+                    ) : (
+                      ": " + selectedContacts.firstname
+                    )}
+                  </td>
+                </tr>
+                <tr className="d-flex">
+                  <td className="col-3 font-bold">Last Name</td>
+                  <td className="col-9">
+                    {editContactFlag ? (
+                      <Input
+                        type="text"
+                        name="lastName"
+                        id="lastName"
+                        defaultValue={selectedContacts.lastname}
+                        onChange={(e) =>
+                          dispatch(
+                            updateContact(
+                              selectedContacts.id,
+                              "lastname",
+                              e.target.value
+                            )
+                          )
+                        }
+                      />
+                    ) : (
+                      ": " + selectedContacts.lastname
+                    )}
+                  </td>
+                </tr>
+                <tr className="d-flex">
+                  <td className="col-3 font-bold">Company</td>
+                  <td className="col-9">
+                    {editContactFlag ? (
+                      <Input
+                        type="text"
+                        name="company"
+                        id="company"
+                        defaultValue={selectedContacts.company}
+                        onChange={(e) =>
+                          dispatch(
+                            updateContact(
+                              selectedContacts.id,
+                              "company",
+                              e.target.value
+                            )
+                          )
+                        }
+                      />
+                    ) : (
+                      ": " + selectedContacts.company
+                    )}
+                  </td>
+                </tr>
+                <tr className="d-flex">
+                  <td className="col-3 font-bold">Department</td>
+                  <td className="col-9">
+                    {editContactFlag ? (
+                      <Input
+                        type="text"
+                        name="department"
+                        id="department"
+                        defaultValue={selectedContacts.department}
+                        onChange={(e) =>
+                          dispatch(
+                            updateContact(
+                              selectedContacts.id,
+                              "department",
+                              e.target.value
+                            )
+                          )
+                        }
+                      />
+                    ) : (
+                      ": " + selectedContacts.department
+                    )}
+                  </td>
+                </tr>
+                <tr className="d-flex">
+                  <td className="col-3 font-bold">Email</td>
+                  <td className="col-9">
+                    {editContactFlag ? (
+                      <Input
+                        type="text"
+                        name="email"
+                        id="email"
+                        defaultValue={selectedContacts.email}
+                        onChange={(e) =>
+                          dispatch(
+                            updateContact(
+                              selectedContacts.id,
+                              "email",
+                              e.target.value
+                            )
+                          )
+                        }
+                      />
+                    ) : (
+                      ": " + selectedContacts.email
+                    )}
+                  </td>
+                </tr>
+                <tr className="d-flex">
+                  <td className="col-3 font-bold">Phone</td>
+                  <td className="col-9">
+                    {editContactFlag ? (
+                      <Input
+                        type="text"
+                        name="phone"
+                        id="phone"
+                        defaultValue={selectedContacts.phone}
+                        onChange={(e) =>
+                          dispatch(
+                            updateContact(
+                              selectedContacts.id,
+                              "phone",
+                              e.target.value
+                            )
+                          )
+                        }
+                      />
+                    ) : (
+                      ": " + selectedContacts.phone
+                    )}
+                  </td>
+                </tr>
+                <tr className="d-flex">
+                  <td className="col-3 font-bold">Address</td>
+                  <td className="col-9">
+                    {editContactFlag ? (
+                      <Input
+                        type="text"
+                        name="address"
+                        id="address"
+                        defaultValue={selectedContacts.address}
+                        onChange={(e) =>
+                          dispatch(
+                            updateContact(
+                              selectedContacts.id,
+                              "address",
+                              e.target.value
+                            )
+                          )
+                        }
+                      />
+                    ) : (
+                      ": " + selectedContacts.address
+                    )}
+                  </td>
+                </tr>
+                
+              </tbody>
+            </Table>
+          </div>
+       
         );
     }
 }
